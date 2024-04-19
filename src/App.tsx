da55a510498b5d8053d345f09f32d08608f09f4e
barbarahellmann <babshellmann@web.de>
2024-04-19 10:21:09 +0200

    first field

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import {ChangeEvent, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+type Input = {
+    name: string
+}
 function App() {
+
+
+    const[formData, setFormdata] = useState<Input>({name: ""})
+
+    function handleOnChange(event: ChangeEvent<HTMLInputElement>) {
+        console.log(event.target.value)
+        setFormdata({name: event.target.value})
+    }
+
+    console.log(formData)
 
   return (
     <>
     <h1>Simple Form with Yup Validation</h1>
         <form>
-            <label>Name</label>
-            <input type={"text"} id={"name"} name={"name"}/>
+            <label>htmlFor={"name"}>Name</label>
+            <input onChange={handleOnChange} type={"text"} id={"name"} name={"name"}/>
         </form>
     </>
   )
